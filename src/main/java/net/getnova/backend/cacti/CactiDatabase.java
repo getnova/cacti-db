e40b396c5b974ea7009df63e8d2a72c51d1a8189
Marcel Koch <koch_marcel@outlook.com>
2020-09-23 20:43:52 +0200

    Removed unused Annotations

--- src/main/java/net/getnova/backend/cacti/CactiDatabase.java
+++ src/main/java/net/getnova/backend/cacti/CactiDatabase.java
@@ -1,6 +1,5 @@
 package net.getnova.backend.cacti;
 
-import lombok.extern.slf4j.Slf4j;
 import net.getnova.backend.api.handler.websocket.WebsocketApiModule;
 import net.getnova.backend.boot.module.Module;
 import net.getnova.backend.jpa.JpaModule;
@@ -8,7 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@Slf4j
 @ComponentScan
 @Module({JpaModule.class, WebsocketApiModule.class})
 @EnableJpaRepositories
